b03e7edb070cd0d103a212e6332178a10e4599bf
twosee <twose@qq.com>
2020-08-31 17:53:59 +0800

    Parallel compilation support

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 __DIR__=$(cd "$(dirname "$0")" || exit 1; pwd); [ -z "${__DIR__}" ] && exit 1
 
-cd "${__DIR__}" && mkdir -p build && cd build && cmake .. && make && ./cat_tests
+
+if [ "$(uname | grep -i darwin)"x != ""x ]; then
+  cpu_count="$(sysctl -n machdep.cpu.core_count)"
+else
+  cpu_count="$(/usr/bin/nproc)"
+fi
+
+cd "${__DIR__}" && mkdir -p build && cd build && \
+cmake .. && \
+make -j"${cpu_count}" && \
+./cat_tests
