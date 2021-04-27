a06b494b8f03f9ba18e694886ce37d4deec41516
twosee <twose@qq.com>
2021-04-27 15:53:17 +0800

    More tests for time module

--- tests/test_cat_time.cc
+++ tests/test_cat_time.cc
@@ -18,6 +18,20 @@
 
 #include "test.h"
 
+TEST(cat_time, nsec)
+{
+    cat_nsec_t s = cat_time_nsec();
+    ASSERT_GT(cat_time_nsec() - s, 0);
+}
+
+TEST(cat_time, msec)
+{
+    cat_msec_t s = cat_time_msec_cached();
+    ASSERT_EQ(cat_time_msec_cached() - s, 0);
+    ASSERT_EQ(cat_time_msleep(1), 0);
+    ASSERT_GT(cat_time_msec_cached() - s, 0);
+}
+
 TEST(cat_time, msleep)
 {
     cat_msec_t s = cat_time_msec();
@@ -42,7 +56,61 @@ TEST(cat_time, nanosleep)
     ASSERT_GE(s, 5);
 }
 
+TEST(cat_time, sleep_zero)
+{
+    cat_coroutine_round_t round = cat_coroutine_get_current_round();
+    EXPECT_EQ(cat_time_sleep(0), 0);
+    EXPECT_GT(cat_coroutine_get_current_round(), round);
+}
+
+static std::string time_format_msec(cat_msec_t msec)
+{
+    char *s = cat_time_format_msec(msec);
+    DEFER(cat_free(s));
+    return std::string(s);
+}
+
+TEST(cat_time, format_msec)
+{
+    ASSERT_EQ(time_format_msec(1ULL), "1ms");
+    ASSERT_EQ(time_format_msec(10ULL), "10ms");
+    ASSERT_EQ(time_format_msec(100ULL), "100ms");
+    ASSERT_EQ(time_format_msec(1000ULL), "1s0ms");
+    ASSERT_EQ(time_format_msec(1001ULL), "1s1ms");
+    ASSERT_EQ(time_format_msec(60ULL * 1000ULL), "1m0s0ms");
+    ASSERT_EQ(time_format_msec(60ULL * 1000ULL + 1000ULL + 1ULL), "1m1s1ms");
+    ASSERT_EQ(time_format_msec(60ULL * 60ULL * 1000ULL), "1h0m0s0ms");
+    ASSERT_EQ(time_format_msec(24ULL * 60ULL * 60ULL * 1000ULL + 60ULL * 60ULL * 1000ULL + 60ULL * 1000ULL + 1000ULL + 1ULL), "1d1h1m1s1ms");
+    ASSERT_EQ(time_format_msec(365ULL * 24ULL * 60ULL * 60ULL * 1000ULL), "365d0h0m0s0ms");
+}
+
+TEST(cat_time, delay_cancel)
+{
+    cat_coroutine_t *coroutine = co([] {
+        ASSERT_EQ(cat_time_delay(CAT_TIMEOUT_FOREVER), CAT_RET_NONE);
+    });
+    ASSERT_TRUE(cat_coroutine_resume(coroutine, nullptr, nullptr));
+}
+
+TEST(cat_time, sleep_cancel)
+{
+    cat_coroutine_t *coroutine = co([] {
+        ASSERT_EQ(cat_time_sleep(1), 1);
+    });
+    ASSERT_TRUE(cat_coroutine_resume(coroutine, nullptr, nullptr));
+    ASSERT_EQ(cat_get_last_error_code(), CAT_ECANCELED);
+}
+
 TEST(cat_time, msleep_cancel)
+{
+    cat_coroutine_t *coroutine = co([] {
+        ASSERT_EQ(cat_time_msleep(1), 1);
+    });
+    ASSERT_TRUE(cat_coroutine_resume(coroutine, nullptr, nullptr));
+    ASSERT_EQ(cat_get_last_error_code(), CAT_ECANCELED);
+}
+
+TEST(cat_time, msleep_cancel2)
 {
     cat_coroutine_t *waiter = cat_coroutine_get_current();
     bool done = false;
@@ -62,9 +130,57 @@ TEST(cat_time, msleep_cancel)
     EXPECT_EQ(done, true);
 }
 
-TEST(cat_time, sleep_zero)
+TEST(cat_time, usleep_cancel)
 {
-    cat_coroutine_round_t round = cat_coroutine_get_current_round();
-    EXPECT_EQ(cat_time_sleep(0), 0);
-    EXPECT_GT(cat_coroutine_get_current_round(), round);
+    cat_coroutine_t *coroutine = co([] {
+        ASSERT_EQ(cat_time_usleep(1000), -1);
+    });
+    ASSERT_TRUE(cat_coroutine_resume(coroutine, nullptr, nullptr));
+    ASSERT_EQ(cat_get_last_error_code(), CAT_ECANCELED);
+}
+
+TEST(cat_time, nanosleep_cancel)
+{
+    cat_coroutine_t *coroutine = co([] {
+        struct timespec rqtp;
+        struct timespec rmtp;
+        rqtp.tv_sec = 1;
+        rqtp.tv_nsec = 1000 * 1000;
+        ASSERT_EQ(cat_time_nanosleep(&rqtp, &rmtp), -1);
+        ASSERT_EQ(rmtp.tv_sec, 1);
+        ASSERT_EQ(rmtp.tv_nsec, 1000 * 1000);
+    });
+    ASSERT_TRUE(cat_coroutine_resume(coroutine, nullptr, nullptr));
+    ASSERT_EQ(cat_get_last_error_code(), CAT_ECANCELED);
+}
+
+TEST(cat_time, nanosleep_inval)
+{
+    struct timespec rqtp;
+    rqtp.tv_sec = -1;
+    rqtp.tv_nsec = 0;
+    ASSERT_EQ(cat_time_nanosleep(&rqtp, nullptr), -1);
+    ASSERT_EQ(cat_get_last_error_code(), CAT_EINVAL);
+    rqtp.tv_sec = 0;
+    rqtp.tv_nsec = -1;
+    ASSERT_EQ(cat_time_nanosleep(&rqtp, nullptr), -1);
+    ASSERT_EQ(cat_get_last_error_code(), CAT_EINVAL);
+    rqtp.tv_sec = 1;
+    rqtp.tv_nsec = 999999999 + 1;
+    ASSERT_EQ(cat_time_nanosleep(&rqtp, nullptr), -1);
+    ASSERT_EQ(cat_get_last_error_code(), CAT_EINVAL);
+}
+
+TEST(cat_time, tv2to)
+{
+    struct timeval tv;
+    ASSERT_EQ(cat_time_tv2to(nullptr), -1);
+    tv.tv_sec = -1;
+    tv.tv_usec = 0;
+    ASSERT_EQ(cat_time_tv2to(&tv), -1);
+    tv.tv_sec = 1;
+    tv.tv_usec = 1;
+    ASSERT_EQ(cat_time_tv2to(&tv), 1000);
+    tv.tv_usec = 1000;
+    ASSERT_EQ(cat_time_tv2to(&tv), 1001);
 }
