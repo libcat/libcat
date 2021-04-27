00c526fef5df757042236d6b254abc60161d5880
twosee <twose@qq.com>
2021-04-27 12:15:31 +0800

    Improve compatibility of run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -3,5 +3,5 @@ __DIR__=$(cd "$(dirname "$0")" || exit 1; pwd); [ -z "${__DIR__}" ] && exit 1
 
 cd "${__DIR__}" && mkdir -p build && cd build && \
 cmake .. && \
-make -j"$(${__DIR__}/tools/cpu_count.sh)" && \
-./cat_tests "$@"
+cmake --build . --target cat_tests -- -j"$(${__DIR__}/tools/cpu_count.sh)" && \
+./cat_tests $*
