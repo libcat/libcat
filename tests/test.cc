cc0fa94800f6ce5beeb60bc1fb3fb80a1c37fe80
twosee <twose@qq.com>
2021-05-13 15:57:43 +0800

    Sanitizer integration in tests

--- tests/test.cc
+++ tests/test.cc
@@ -258,3 +258,19 @@ TEST(test, defer)
     DEFER(ASSERT_TRUE(foo));
     foo = true;
 }
+
+extern "C"
+{
+    void __ubsan_on_report()
+    {
+        FAIL() << "Encountered an undefined behavior sanitizer error";
+    }
+    void __asan_on_error(void)
+    {
+        FAIL() << "Encountered an address sanitizer error";
+    }
+    void __tsan_on_report()
+    {
+        FAIL() << "Encountered a thread sanitizer error";
+    }
+}
