05e5405bcb95fd2756cb67db08da4a4039e3a77b
dixyes <dixyes@gmail.com>
2021-06-04 17:37:39 +0800

    Improve shell usage
    I reversed "$@" and $*, this fix also makes codacy happy.

--- run-tests.sh
+++ run-tests.sh
@@ -4,4 +4,4 @@ __DIR__=$(cd "$(dirname "$0")" || exit 1; pwd); [ -z "${__DIR__}" ] && exit 1
 cd "${__DIR__}" && mkdir -p build && cd build && \
 cmake .. && \
 cmake --build . --target cat_tests -- -j"$(${__DIR__}/tools/cpu_count.sh)" && \
-./cat_tests $*
+exec ./cat_tests "$@"
