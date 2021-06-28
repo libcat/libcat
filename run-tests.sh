1c39c4634cafbf349149bee675cb680570a7d3cf
twosee <twose@qq.com>
2021-06-28 14:21:11 +0800

    Enable ASan for tests by default

--- run-tests.sh
+++ run-tests.sh
@@ -2,6 +2,6 @@
 __DIR__=$(cd "$(dirname "$0")" || exit 1; pwd); [ -z "${__DIR__}" ] && exit 1
 
 cd "${__DIR__}" && mkdir -p build && cd build && \
-cmake .. && \
+cmake -DASAN=ON .. && \
 cmake --build . --target cat_tests -- -j"$(${__DIR__}/tools/cpu_count.sh)" && \
 exec ./cat_tests "$@"
