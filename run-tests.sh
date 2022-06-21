99af10c65791307c1f779e7bd326d2ac4a65606c
twosee <twose@qq.com>
2022-06-21 12:04:35 +0800

    Enhance run-tests.sh to support set options by env

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 __DIR__=$(cd "$(dirname "$0")" || exit 1; pwd); [ -z "${__DIR__}" ] && exit 1
 
-cd "${__DIR__}" && mkdir -p build && cd build && \
-cmake -DASAN=ON .. && \
-cmake --build . --target cat_tests -- -j"$(${__DIR__}/tools/cpu_count.sh)" && \
+set -e
+  # go to workspace
+  cd "${__DIR__}"
+  mkdir -p build
+  cd build
+
+  # solve definitions
+  if [ "${LIBCAT_BUILD_TYPE-Debug}"x == "Debug"x ]; then
+    LIBCAT_ENABLE_ASAN_DEFAULT_VALUE="ON"
+  fi
+  cmake \
+    -DCMAKE_BUILD_TYPE="${LIBCAT_BUILD_TYPE-Debug}" \
+    -DLIBCAT_USE_THREAD_CONTEXT="${LIBCAT_USE_THREAD_CONTEXT-OFF}" \
+    -DLIBCAT_ENABLE_ASAN="${LIBCAT_ENABLE_ASAN-${LIBCAT_ENABLE_ASAN_DEFAULT_VALUE}}" \
+    -DLIBCAT_WITH_VALGRIND="${LIBCAT_WITH_VALGRIND-ON}" \
+    -DLIBCAT_USE_THREAD_LOCAL="${LIBCAT_USE_THREAD_LOCAL-OFF}" \
+    -DLIBCAT_USE_THREAD_KEY="${LIBCAT_USE_THREAD_KEY-OFF}" \
+    -DLIBCAT_ENABLE_OPENSSL="${LIBCAT_ENABLE_OPENSSL-ON}" \
+    -DLIBCAT_ENABLE_CURL="${LIBCAT_ENABLE_CURL-ON}" \
+    ..
+
+  # build tests
+  cmake --build . --target cat_tests -- -j"$(${__DIR__}/tools/cpu_count.sh)"
+set +e
+
+# run tests
 exec ./cat_tests "$@"
